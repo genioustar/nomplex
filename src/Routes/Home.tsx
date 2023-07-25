085530042ce33379b3d7cff407f1294ad3e4bdaa
genioustar <4574gkdl>
2023-07-25 09:00:37 +0900

    헤더만들고 빨간점 애니메이션으로 만듬

---
+++ src/Routes/Home.tsx
@@ -0,0 +1,10 @@
+function Home() {
+  console.log("home");
+  return (
+    <div style={{ backgroundColor: "black", height: "200vh" }}>
+      <h1 style={{ color: "red", fontSize: "500px" }}>홈</h1>
+    </div>
+  );
+}
+
+export default Home;
---
+++ src/Routes/Tv.tsx
@@ -0,0 +1,6 @@
+function Tv() {
+  console.log("tv");
+  return <h1 style={{ color: "red", fontSize: "500px" }}>Tv</h1>;
+}
+
+export default Tv;
